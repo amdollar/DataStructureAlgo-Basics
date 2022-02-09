902450e60abf01c433a366dc8eb5afac72a0c5ff
Anurag <anuragawasthi020@gmail.com>
2022-02-09 20:23:27 +0530

    Adding code for calculating power of given number

---
+++ src/com/java/Recursion/PowerFuncation.java
@@ -0,0 +1,35 @@
+package com.java.Recursion;
+
+public class PowerFuncation {
+
+	public static void main(String[] args) {
+		int n = 3;
+		int p = 4;
+//		int res = findPower(n, p);
+		int res1 = findPowerOptimized(n, p);
+
+		System.out.println(res1);
+//		System.out.println(res);
+	}
+
+	private static int findPower(int n, int p) {
+		if (p == 1) {
+			return n;
+		}
+		return n * findPower(n, p - 1);
+	}
+
+	// find power in optimized manner
+	private static int findPowerOptimized(int n, int p) {
+		if (p == 0) {
+			return 1;
+		}
+		int res = findPowerOptimized(n, p / 2);
+		int temp = res * res;
+		if (p % 2 != 0) {
+			return n * temp;
+		}
+		return temp;
+	}
+
+}
