e49a928f01eac173012ae70e8633c61595fbebbe
Anurag <anuragawasthi020@gmail.com>
2022-02-09 16:23:13 +0530

    Updated code for finding array is sorted or not using recursion

--- src/com/java/Recursion/SortedArray.java
+++ src/com/java/Recursion/SortedArray.java
@@ -5,6 +5,7 @@ public class SortedArray {
 	public static void main(String[] args) {
 		int arr[] = new int[] { 1, 2, 13, 4, 5 };
 		System.out.println(isSorted(arr, arr.length));
+		System.out.println(isSorted(arr, 0, arr.length));
 
 	}
 
@@ -14,6 +15,17 @@ public class SortedArray {
 			return true;
 
 		if (arr[length - 2] < arr[length - 1] && isSorted(arr, length - 1)) {
+			return true;
+		}
+		return false;
+	}
+
+	// method 2
+	private static boolean isSorted(int arr[], int i, int length) {
+		if (i == length - 1) {
+			return true;
+		}
+		if (arr[i] < arr[i + 1] && isSorted(arr, i + 1, length)) {
 			return true;
 		}
 		return false;
