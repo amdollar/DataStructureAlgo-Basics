1572744fb215bc339068b1a4c7ce6f8bd2da73d2
Anurag <anuragawasthi020@gmail.com>
2022-03-01 00:16:26 +0530

    Modified Tries for multiple words and queries.

--- src/com/java/Tries/TriesDriver.java
+++ src/com/java/Tries/TriesDriver.java
@@ -3,11 +3,16 @@ package com.java.Tries;
 public class TriesDriver {
 
 	public static void main(String[] args) {
+		String[] words = new String[] { "Anurag", "Anu", "King", "Banana", "Bana" };
+		String[] queries = new String[] { "Anurag", "An", "King", "Ba", "Banan" };
 		Tries tries = new Tries();
-		tries.push("Anurag");
-		boolean res = tries.search("Anurag");
+		for (String word : words) {
+			tries.push(word);
+		}
+		for (String temp : queries) {
+			System.out.println(tries.search(temp));
+		}
 
-		System.out.println(tries.search("Anura"));
 	}
 
 }
