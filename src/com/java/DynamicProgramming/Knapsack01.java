408b654b421719302a575ee21e9b0ddbbae8f84f
Anurag <anuragawasthi020@gmail.com>
2022-03-02 14:59:42 +0530

    Added memorization in 0/1 knapsack problem

--- src/com/java/DynamicProgramming/Knapsack01.java
+++ src/com/java/DynamicProgramming/Knapsack01.java
@@ -1,6 +1,16 @@
 package com.java.DynamicProgramming;
 
 public class Knapsack01 {
+	// this array would be initialized for the purpose of memorization of variables
+	// which are changing frequently throughout the recursion call
+	static int dp[][] = new int[1000][1000];
+	static {
+		for (int i = 0; i < dp.length; i++) {
+			for (int j = 0; j < dp[i].length; j++) {
+				dp[i][j] = -1;
+			}
+		}
+	}
 
 	public static void main(String[] args) {
 		int[] weights = new int[] { 10, 20, 30 };
@@ -15,15 +25,19 @@ public class Knapsack01 {
 		if (capacity == 0 || len == 0)
 			return 0;
 
+		// this array will return the value if already calculated.
+		if (dp[capacity][len] != -1) {
+			return dp[capacity][len];
+		}
 		// if we want to include the item in the bag or not: based on:
 		// if item weight is < capacity and if I want to include(if it gives me max
 		// profit)
 		// and if the weight> capacity then discard this item from the input arr
 		if (capacity >= weights[len]) {
-			return Math.max(price[len] + knapsack(weights, price, capacity - weights[len], len - 1),
+			return dp[capacity][len] = Math.max(price[len] + knapsack(weights, price, capacity - weights[len], len - 1),
 					knapsack(weights, price, capacity, len - 1));
 		} else {
-			return knapsack(weights, price, capacity, len - 1);
+			return dp[capacity][len] = knapsack(weights, price, capacity, len - 1);
 		} // (capacity < weights[len])
 	}
 }
