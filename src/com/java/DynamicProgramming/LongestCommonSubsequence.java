f54b58e4eec224ddeceab20b7847d623abbe1d88
Anurag <anuragawasthi020@gmail.com>
2022-08-30 13:20:26 +0530

    Updated code as top-down dp

--- src/com/java/DynamicProgramming/LongestCommonSubsequence.java
+++ src/com/java/DynamicProgramming/LongestCommonSubsequence.java
@@ -5,9 +5,9 @@ public class LongestCommonSubsequence {
 	static int[][] t = new int[20][20];
 
 	public static void main(String[] args) {
-		String text1 = "abc";
-		String text2 = "def";
-		fillMatrix();
+		String text1 = "abcde";
+		String text2 = "abc";
+//		fillMatrix();
 		int lcsl = longestCommonSubsequence(text1, text2);
 		System.out.println(lcsl);
 	}
@@ -21,28 +21,42 @@ public class LongestCommonSubsequence {
 	}
 
 	public static int longestCommonSubsequence(String text1, String text2) {
-		return finalMethod(text1, text1.length() - 1, text2, text2.length() - 1);
+		return finalMethod(text1, text1.length(), text2, text2.length());
 
 	}
 
 	private static int finalMethod(String text1, int l1, String text2, int l2) {
-		if (l1 == -1 || l2 == -1) {
-			return 0;
+		int t[][] = new int[l1 +1][l2+1];
+		for (int i = 0; i <= l1; i++) {
+			t[i][0] = 0;
 		}
-		if (t[l1][l2] != -1) {
-			return t[l1][l2];
+		for (int i = 0; i <= l2; i++) {
+			t[0][i] = 0;
+		}
+		for (int i = 1; i <= l1; i++) {
+			for (int j = 1; j <= l2; j++) {
+				if (text1.charAt(i - 1) == text2.charAt(j - 1)) {
+					t[i][j] = 1 + t[i - 1][j - 1];
+				} else {
+					t[i][j] = Math.max(t[i - 1][j], t[i][j - 1]);
+				}
+			}
 		}
 
-		if (text1.charAt(l1) == text2.charAt(l2)) {
-			t[l1][l2] = 1 + finalMethod(text1, l1 - 1, text2, l2 - 1);
-			return t[l1][l2];
-		} else {
-			t[l1][l2] = Math.max(finalMethod(text1, l1 - 1, text2, l2), finalMethod(text1, l1, text2, l2 - 1));
-			return t[l1][l2];
-		}
-
-//		return t[l1][l2];
+		return t[l1][l2];
 	}
+
+	// memoization
+	/*
+	 * private static int finalMethod(String text1, int l1, String text2, int l2) {
+	 * if (l1 == -1 || l2 == -1) { return 0; } if (t[l1][l2] != -1) { return
+	 * t[l1][l2]; }
+	 * 
+	 * if (text1.charAt(l1) == text2.charAt(l2)) { t[l1][l2] = 1 +
+	 * finalMethod(text1, l1 - 1, text2, l2 - 1); return t[l1][l2]; } else {
+	 * t[l1][l2] = Math.max(finalMethod(text1, l1 - 1, text2, l2),
+	 * finalMethod(text1, l1, text2, l2 - 1)); return t[l1][l2]; } }
+	 */
 
 	/*
 	 * private static int finalMethod(String text1, int length, String text2, int
@@ -53,9 +67,4 @@ public class LongestCommonSubsequence {
 	 * length, text2, length2 - 1)); } }
 	 */
 
-	// memoization
-
 }
-
-//"aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"
-//"aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"
