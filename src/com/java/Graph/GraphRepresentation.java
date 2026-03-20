0e9089b2de2d2c4e3f7914a4fb11ffc7950285e9
Anurag <anuragawasthi020@gmail.com>
2026-03-20 22:21:07 +0530

    Adding graph sub module

---
+++ src/com/java/Graph/GraphRepresentation.java
@@ -0,0 +1,17 @@
+package com.java.Graph;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class GraphRepresentation {
+
+	public static void main(String[] args) {
+		// Define the total number of edges and vertex
+		int n = 3, m= 3;
+		List<List<Integer>> graph = new ArrayList<>();
+		
+		
+
+	}
+
+}
