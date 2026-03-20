5f25af8e72fc977ff3cd3b85b45a570201ff0d81
Anurag <anuragawasthi020@gmail.com>
2026-03-20 23:04:28 +0530

    Full code for printing Un-directed graph

--- src/com/java/Graph/GraphRepresentation.java
+++ src/com/java/Graph/GraphRepresentation.java
@@ -7,11 +7,29 @@ public class GraphRepresentation {
 
 	public static void main(String[] args) {
 		// Define the total number of edges and vertex
-		int n = 3, m= 3;
+		int n = 3, m = 3;
 		List<List<Integer>> graph = new ArrayList<>();
-		
-		
+
+		for (int i = 0; i <= n; i++) {
+			graph.add(new ArrayList<Integer>());
+		}
+
+		// 1->2
+		graph.get(1).add(2);
+		graph.get(2).add(1);
+
+		// 2->3
+		graph.get(2).add(3);
+		graph.get(3).add(2);
+
+		for (int i = 1; i < graph.size(); i++) {
+			for (int j = 0; j < graph.get(i).size(); j++) {
+				System.out.print(graph.get(i).get(j)+ " ");
+			}
+			System.out.println();
+		}
 
 	}
 
 }
+
