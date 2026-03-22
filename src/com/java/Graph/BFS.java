0ed35291e103a842cf55e6d2b32e41c3ab33bf17
Anurag <anuragawasthi020@gmail.com>
2026-03-22 17:56:48 +0530

    BFS code completed

--- src/com/java/Graph/BFS.java
+++ src/com/java/Graph/BFS.java
@@ -1,7 +1,9 @@
 package com.java.Graph;
 
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Queue;
 import java.util.Scanner;
 
 public class BFS {
@@ -18,7 +20,7 @@ public class BFS {
 			adj.add(new ArrayList<>());
 		}
 
-		for (int i = 0; i <= vertex; i++) {
+		for (int i = 0; i <= edges; i++) {
 			// take the vertexs as input to define edges b/w two of them
 			// 2-3 3-2
 			int v = sc.nextInt();
@@ -26,7 +28,37 @@ public class BFS {
 
 			adj.get(v).add(u);
 			adj.get(u).add(v);
-			printGraph(adj, vertex, edges);
+		}
+		printGraph(adj, vertex, edges);
+
+		bfs(adj, vertex + 1, 1);
+
+	}
+
+	private static void bfs(List<List<Integer>> graph, int vertex, int startNode) {
+		// Queue for BFS and visited array for tracking.
+		Queue<Integer> queue = new LinkedList<>();
+		boolean visited[] = new boolean[vertex];
+
+		// Mark start node as visited and add it in the queue for further processing.
+		visited[startNode] = true;
+		queue.offer(startNode);
+		System.out.println("BFS traversal:");
+
+		// Process the queue while it is not empty
+		while (!queue.isEmpty()) {
+			// grab the first element of the queue
+			int current = queue.poll();
+			System.out.print(current + " ");
+
+			// Grab the adj list attached to this node in adj graph
+			for (int element : graph.get(current)) {
+				if (!visited[element]) {
+					visited[element] = true;
+					queue.add(element);
+				}
+			}
+
 		}
 
 	}
