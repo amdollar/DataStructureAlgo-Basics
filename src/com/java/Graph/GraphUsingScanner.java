51b57568c2933975b28bdad565049cae9d78a084
Anurag <anuragawasthi020@gmail.com>
2026-03-22 14:15:06 +0530

    Printintg graph using external inputs

---
+++ src/com/java/Graph/GraphUsingScanner.java
@@ -0,0 +1,21 @@
+package com.java.Graph;
+
+public class GraphUsingScanner {
+
+	public static void main(String[] args) {
+		
+		// Take input of no of vertex and edges
+		
+		
+		
+		// Create the empty Adj List with number of edges to hold the internal connected nodes
+		
+		
+		// Take input for read each edge input (Vertex and then Edge)
+		
+		
+		// Print graph
+
+	}
+
+}
