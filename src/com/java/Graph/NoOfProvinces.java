52af6de179e2a0ab715016f68817affa174f0213
Anurag <anuragawasthi020@gmail.com>
2026-03-26 23:44:09 +0530

    Code to find number of provinces in a Graph

---
+++ src/com/java/Graph/NoOfProvinces.java
@@ -0,0 +1,11 @@
+package com.java.Graph;
+
+//A province is a group of directly or indirectly connected cities and no other cities outside of the group.
+public class NoOfProvinces {
+
+	public static void main(String[] args) {
+		
+
+	}
+
+}
