2d4ab11fc13ce933a8d772e6a8f0670a356c6c8e
Anurag <anuragawasthi020@gmail.com>
2026-04-18 00:33:50 +0530

    Code complation

--- src/com/java/Graph/FloydWershall42.java
+++ src/com/java/Graph/FloydWershall42.java
@@ -1,5 +1,7 @@
 package com.java.Graph;
 
+import java.util.Arrays;
+
 //https://takeuforward.org/data-structure/floyd-warshall-algorithm-g-42
 public class FloydWershall42 {
 
@@ -10,12 +12,50 @@ public class FloydWershall42 {
 
 		int[][] matrix = { { 0, 1, 43 }, { 1, 0, 6 }, { -1, -1, 0 } };
 
-		int n = matrix[0].length;
-		int m = matrix.length;
+		int n = matrix.length;
+//		int m = matrix.length;
 		// each inner array represents a row in matrix
 
-		// Step 1. have one weight ajd array:
-		int[][] cost = new int[n][m];
+		// since question asks for in-place, we are modifying the same input matrix
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < n; j++) {
+				if (matrix[i][j] == -1) {
+//					matrix[i][j] = Integer.MAX_VALUE;// This is causing integer max issue. so changing to other large
+					// value
+					matrix[i][j] = (int) 1e9;
+				}
+				if (i == j) {
+					matrix[i][j] = 0;
+				}
+			}
+		}
+
+		// Now start filling the cost matrix with calculations, via each of the vertex
+
+		for (int via = 0; via < n; via++) {
+			for (int i = 0; i < n; i++) {
+				for (int j = 0; j < n; j++) {
+					matrix[i][j] = Math.min(matrix[i][via] + matrix[via][j], matrix[i][j]);
+				}
+
+			}
+		}
+
+		// In case there is no way to reach convert it back to -1 as in input
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < n; j++) {
+				if (matrix[i][j] == (int) 1e9) {
+					matrix[i][j] = -1;
+				}
+			}
+		}
+
+		for (int[] temp : matrix) {
+			for (int i : temp) {
+				System.out.print(" " + i);
+			}
+			System.out.println();
+		}
 
 	}
 
